f776762fadb2ffaee98d5e1192dda78404c6556f
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-11-18 16:38:08 +0530

    Modify SignIn SignUp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignIn = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <>
       <Header />
@@ -15,14 +20,28 @@ const Login = () => {
             className="w-full h-full object-cover"
           />
         </div>
-
-        <div className="absolute inset-0 bg-black bg-opacity-40 -z-5"></div> {/* This creates the overlay */}
-
-
+        <div className="absolute inset-0 bg-black bg-opacity-40 -z-5"></div>{" "}
+        {/* This creates the overlay */}
         {/* Form container */}
         <div className="relative bg-black bg-opacity-60 px-10 py-6 rounded-lg w-[400px]">
-          <label className="text-white text-2xl font-bold">Sign In</label>
+          <h1 className="text-white text-2xl font-bold">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h1>
           <form className="flex flex-col items-center">
+            {!isSignInForm && (
+              <input
+                type="text"
+                placeholder="First Name"
+                className="w-full h-10 m-2 p-2 rounded-md opacity-60"
+              />
+            )}
+            {!isSignInForm && (
+              <input
+                type="text"
+                placeholder="Last Name"
+                className="w-full h-10 m-2 p-2 rounded-md opacity-60"
+              />
+            )}
             <input
               type="text"
               placeholder="Email or phone number"
@@ -34,11 +53,14 @@ const Login = () => {
               className="w-full h-10 m-2 p-2 rounded-md opacity-60"
             />
             <button className="w-full h-10 m-2 p-2 bg-red-800 text-white rounded-md hover:bg-red-600">
-              Sign In
+              {isSignInForm ? "Sign In" : "Sign Up"}
             </button>
             <div className="flex justify-between items-center w-full">
               <div>
-                <input type="checkbox" className="my-2 mr-2 hover:cursor-pointer" />
+                <input
+                  type="checkbox"
+                  className="my-2 mr-2 hover:cursor-pointer"
+                />
                 <span className="text-white">Remember me</span>
               </div>
               <div>
@@ -48,11 +70,19 @@ const Login = () => {
               </div>
             </div>
             <div className="flex items-center w-full gap-2 mt-4">
-              <span className="text-gray-400">New to Netflix?</span>
-              <Link to="/browse" className="text-gray-300 hover:text-white">
+              <span className="text-gray-400 hover:cursor-default">
+                {isSignInForm ? "New to Netflix?" : "Already registered!"}
+              </span>
+              {/* <Link to="/browse" className="text-gray-300 hover:text-white" onClick={toggleSignIn}>
                 Sign up now
-              </Link>
-              </div>
+              </Link> */}
+              <h1
+                className="text-gray-300 hover:text-white hover:cursor-pointer"
+                onClick={toggleSignIn}
+              >
+                {isSignInForm ? "Sign Up" : "Sign In"}
+              </h1>
+            </div>
           </form>
         </div>
       </div>
